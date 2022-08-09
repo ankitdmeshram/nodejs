d2271c80726fb7ab6dfa6e8943369dbcce4616f2
Ankit Meshram <ankit@brokod.com>
2022-08-09 09:32:08 +0530

    api

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name: 'ankit', email: 'ankit@borkod.com'},
+    {name: 'ankit2', email: 'ankit@borkod.com2'},
+    {name: 'ankit3', email: 'ankit@borkod.com3'}
+]
+module.exports = data;
